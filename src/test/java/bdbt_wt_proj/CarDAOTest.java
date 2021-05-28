8d4372c22ff09f14af8fcb36c8e7bd9b57ff0b7f
Arkadiusz Sowa <a.sowa@stud.elka.pw.edu.pl>
2021-05-28 15:47:49 +0200

    CarDaoTest

---
+++ src/test/java/bdbt_wt_proj/CarDAOTest.java
@@ -0,0 +1,30 @@
+package bdbt_wt_proj;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarDAOTest extends Object {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void list() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void save() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+}
