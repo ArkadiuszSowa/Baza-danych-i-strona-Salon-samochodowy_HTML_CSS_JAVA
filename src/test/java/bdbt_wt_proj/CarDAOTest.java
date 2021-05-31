1d2e96f67cea838eace1a2e1edb501629746709d
Arkadiusz Sowa <a.sowa@stud.elka.pw.edu.pl>
2021-05-31 17:00:36 +0200

    DAOTEst funkcje list i setUp

--- src/test/java/bdbt_wt_proj/CarDAOTest.java
+++ src/test/java/bdbt_wt_proj/CarDAOTest.java
@@ -1,15 +1,31 @@
 package bdbt_wt_proj;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarDAOTest extends Object {
 
+    private CarDAO dao;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        DriverManagerDataSource dataSource=new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:xe");
+        dataSource.setUsername("asowa");
+        dataSource.setPassword("asowa");
+        dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+
+        dao = new CarDAO(new JdbcTemplate(dataSource));
     }
 
     @org.junit.jupiter.api.Test
     void list() {
+        List<Car> listCar = dao.list();
+        assertTrue(listCar.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
