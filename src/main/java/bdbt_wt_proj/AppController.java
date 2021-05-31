5ceea343ee6cb1311b7f100a4e068f0b88622c2c
burasjan <janusz.buras@orange.com>
2021-05-31 17:45:48 +0200

    Pierwszy odcinek od Pabla

---
+++ src/main/java/bdbt_wt_proj/AppController.java
@@ -0,0 +1,21 @@
+package bdbt_wt_proj;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+public class AppController {
+
+    @Autowired
+ private CarDAO dao;
+
+
+ @RequestMapping("/")
+ public String viewHomePage(Model model){
+     List<Car> listCar = dao.list();
+     model.addAttribute("listCar", listCar);
+     return "index";
+ }
+}
